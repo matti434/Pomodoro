6c8bd1863690478f1c74ebfd464e8dc4599f764c
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-19 18:25:39 -0300

    fix: resolve issues and ensure the app work as expected

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const inputStudy = document.getElementById("inputStudy");
+const inputBreak = document.getElementById("inputBreak");
+const stateDisplay = document.getElementById("stateDisplay");
+const timerDisplay = document.getElementById("timerDisplay");
+const startButton = document.getElementById("startButton");
+const pauseButton = document.getElementById("pauseButton");
+const resetButton = document.getElementById("resetButton");
 // primero creamos una variable para saber si es true "estudio" o false "descanso"
-let isStudy=true;
-let timeLeft=0; // segundos restantes
-let timerInvertal = null; // referencia del setInterval  
+let isStudy = true;
+let timeLeft = 0; // segundos restantes
+let timerInterval = null; // referencia del setInterval
 
 /*
 let timerInterval = null	No hay timer activo
@@ -11,48 +17,68 @@ clearInterval(timerInterval)	Detener el timer actual
 timerInterval = null	Marcar que ya no hay ningún timer activo
 */
 
-function updateDisplay(){
-    const minutes = Math.floor(timeLeft/60).toString().padStart(2,'0');
-    const seconds = (timeLeft%60).toString().padStart(2,'0');// redondea para abajo, saca el resto lo vuelve una cadena y le agrega cerros al comienzo si tiene menos de 2 caracteres
-    timerDisplat.textContent = `${minutes}:${seconds}`;
-    stateDisplay.textContent = isStudy ? "Study Time" : "Break time";
+function updateDisplay() {
+  const minutes = Math.floor(timeLeft / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (timeLeft % 60).toString().padStart(2, "0"); // redondea para abajo, saca el resto lo vuelve una cadena y le agrega cerros al comienzo si tiene menos de 2 caracteres
+  timerDisplay.textContent = `${minutes}:${seconds}`;
+  stateDisplay.textContent = isStudy ? "Study Time" : "Break time";
 }
 
-function startTimer(){
-    if(!timerInvertal){ // si no hay temporizador corriendo //!timerInterval será true si timerInterval es null, undefined o 0.
-        if(timeLeft<=0){ //Si el tiempo restante es 0 o menos, quiere decir que el temporizador está "vacío".
-            timeLeft= parseInt(isStudy ? inputStudy.value : inputBreak.value)*60;
-        }
+function startTimer() {
+  if (!timerInterval) {
+    // si no hay temporizador corriendo //!timerInterval será true si timerInterval es null, undefined o 0.
+    if (timeLeft <= 0) {
+      //Si el tiempo restante es 0 o menos, quiere decir que el temporizador está "vacío".
+      timeLeft = parseInt(isStudy ? inputStudy.value : inputBreak.value) * 60;
     }
-    timerInvertal= setInterval(() => {
-        timeLeft--;
+
+    
+    timerInterval = setInterval(() => {
+      timeLeft--;
+      updateDisplay();
+
+      if (timeLeft <= 0) {
+        clearInterval(timerInterval); //Se detiene el setInterval actual (clearInterval)
+        timerInterval = null; //Se pone timerInterval = null para permitir crear uno nuevo
+        isStudy = !isStudy; // cambia entre estudio y descanso
+        startTimer(); // Comienza el siguiente ciclo automaticamente
+      }
+    }, 1000);
+  }
+}
+
+function pauseTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+}
+
+function resetTimer() {
+  pauseTimer();
+  isStudy = true;
+  timeLeft = parseInt(inputStudy.value) * 60;
+  updateDisplay();
+}
+
+inputStudy.addEventListener("input",() =>{
+    if(isStudy && !timerInterval){
+        timeLeft = parseInt(inputStudy.value)*60 || 0;
         updateDisplay();
-        
-        if(timeLeft<=0){
-            clearInterval(timerInvertal);//Se detiene el setInterval actual (clearInterval)
-            timerInvertal=null;//Se pone timerInterval = null para permitir crear uno nuevo
-            isStudy=!isStudy; // cambia entre estudio y descanso
-            startTimer(); // Comienza el siguiente ciclo automaticamente
-        }
-    },1000);
-}
+    }
+});
 
-function pauseTimer(){
-    clearInterval(timerInvertal);
-    timerInvertal=null
-}
-
-function resetTimer(){
-    pauseTimer();
-    isStudy=true;
-    timeLeft = parseInt(inputStudy.value)*60;
+inputBreak.addEventListener("input", () => {
+  if (!isStudy && !timerInterval) {
+    timeLeft = parseInt(inputBreak.value) * 60 || 0;
     updateDisplay();
-}
+  }
+});
 
-startButton.addEventListener('click', startTimer);
-pauseButton.addEventListener('click',pauseTimer);
-resetButton.addEventListener('click',resetTimer);
+startButton.addEventListener("click", startTimer);
+pauseButton.addEventListener("click", pauseTimer);
+resetButton.addEventListener("click", resetTimer);
 
 //inicializa el display
-timeLeft=parseInt(inputStudy.value)*60;
+timeLeft = parseInt(inputStudy.value) * 60;
 updateDisplay();
